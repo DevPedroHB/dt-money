8316d99751423e43b0160825126176ffea3817fe
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2023-01-08 03:21:42 -0300

    table transactions

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -6,7 +6,7 @@ interface SummaryCardProps {
 
 export const SummaryContainer = styled.section`
   width: 100%;
-  max-width: 1120px;
+  max-width: 70rem;
   margin: 0 auto;
   padding: 0 1.5rem;
   display: grid;
